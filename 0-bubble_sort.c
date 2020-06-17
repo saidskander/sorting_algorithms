99798ff985cd82f2c8c0e089e5611fb3a87e9f73
saidskander <askander199h@outlook.com>
2020-06-17 17:15:16 +0100

    Done

---
+++ 0-bubble_sort.c
@@ -0,0 +1,33 @@
+#include "sort.h"
+/**
+ * bubble_sort - bubble_sort algo.
+ * @array : int *array
+ * @size : size_t size
+ */
+void bubble_sort(int *array, size_t size)
+{
+int x = 0;
+int box = 0;
+int  path = 0, size_path = size;
+if (size == 0 || size == 1)
+return;
+for (; x < size_path; x++)
+{
+box = 0;
+if (array[x] > array[x + 1])
+{
+path = array[x];
+array[x] = array[x + 1];
+array[x + 1] = path;
+box = 1;
+print_array(array, size);
+}
+if (x == size_path - 2)
+{
+if (box == 0)
+break;
+size_path--;
+x = -1;
+}
+}
+}
